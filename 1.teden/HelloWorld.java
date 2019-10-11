ded158309237efaac519b935bd92db8ac08984b6
Uroš Zoretič <uz1734@student.uni-lj.si>
2019-10-11 13:25:33 +0200

    week one

---
+++ 1.teden/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println(Arrays.toString(args));
+	}
+
+}
