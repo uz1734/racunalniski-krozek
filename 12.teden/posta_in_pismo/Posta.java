e77fb3e44f3c2f7a27c84402c48f93af58b46bec
Uroš Zoretič <uz1734@student.uni-lj.si>
2020-01-10 15:15:14 +0100

    objekti

--- 12.teden/posta_in_pismo/Posta.java
+++ 12.teden/posta_in_pismo/Posta.java
@@ -1,2 +1,20 @@
 public class Posta{
+	
+	private int stevilka;
+	private String naziv;
+	
+	public Posta(int stevilka, String naziv){
+		this.stevilka = stevilka;
+		this.naziv = naziv;
+	}
+	
+	public int vrniStevilko(){
+		return this.stevilka;
+	}
+	public String vrniNaziv(){
+		return this.naziv;
+	}
+	public String toString(){
+		return this.stevilka+" "+this.naziv;
+	}
 }
