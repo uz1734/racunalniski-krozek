a5293cd55fef1e9da8d58f0e5886abe34100fb25
Uroš Zoretič <uz1734@student.uni-lj.si>
2020-02-07 15:59:09 +0100

    15. teden krozka resitve vaj

--- 15.teden/naloga01_krozki/Krozek.java
+++ 15.teden/naloga01_krozki/Krozek.java
@@ -23,4 +23,51 @@ public class Krozek {
     public void nastaviClane(Ucenec[] clani) {
         this.clani = clani;
     }	
+	
+	// getter, ki vrne dan kdaj se krožek izvaja
+	public int vrniDan(){
+		return this.dan;
+	}
+	// getter, ki vrne uro izvajanja krožka
+	public int vrniUra(){
+		return this.zacUra;
+	}
+	// vrne vse clane krožka
+	public Ucenec[] vrniClane(){
+		return this.clani;
+	}
+	
+		// vrni samo razlicne dejavnosti, ki ga obiskujejo clani tega krozka.... 
+	public int steviloRazlicnihDejavnosti(){
+		int maxDolzina = 0;
+		for(int i = 0; i < this.clani.length; i++){
+			Krozek [] krozki = this.clani[i].krozki;
+			for(int j = 0; j < krozki.length; j++){
+				maxDolzina++;
+			}
+		}
+		
+		String [] dejavnosti = new String [maxDolzina];
+		
+		int d = 0;
+		
+		for(int i = 0; i < this.clani.length; i++){
+			Krozek[] krozki = this.clani[i].krozki;
+				for(int j = 0; j < krozki.length; j++){
+				dejavnosti[d] = krozki[j].dejavnost;
+				d++;
+			}
+		}
+		
+		Arrays.sort(dejavnosti);
+		int stRazlicnih = 1;
+		for(int i = 1; i < dejavnosti.length; i++){
+			if(! dejavnosti[i].equals(dejavnosti[i - 1])){
+				stRazlicnih++;
+			}
+		}	
+		return stRazlicnih;
+	}	
+	
+	
 }
--- 15.teden/naloga01_krozki/Ucenec.java
+++ 15.teden/naloga01_krozki/Ucenec.java
@@ -18,4 +18,37 @@ public class Ucenec {
     public void nastaviKrozke(Krozek[] krozki) {
         this.krozki = krozki;
     }
+	
+		
+	public String toString(){
+		return this.ip+", "+this.razred+". razred";
+	}
+	
+	// ali ima ucenec se cas za dolocen podani krozek. Upostevamo da vsak krozek traja 2uri
+	public boolean imaCasZa(Krozek k){
+		for(int i = 0; i < this.krozki.length; i++){
+			if(this.krozki[i].vrniDan() == k.vrniDan()){
+				if(Math.abs(krozki[i].vrniUra() - k.vrniUra()) < 2)
+					return false;
+			}
+		}
+		return true;
+	}
+	
+	public int steviloSosolcevVKrozku(Krozek k) {
+		Ucenec[] ucenciKrozka = k.vrniClane();
+		
+		int stSosolcev = 0;
+		
+		for (int i = 0; i < ucenciKrozka.length; i++) {
+			
+			if (ucenciKrozka[i].razred == this.razred && ucenciKrozka[i] != this) {
+				stSosolcev++;
+			}
+			
+		}
+		
+		return stSosolcev;
+	}
+	
 }
